5cce9aaf4fe83773efcf114542972f5d33831905
Crazy Dev John <56544124+CrazyDevJohn@users.noreply.github.com>
2026-01-30 07:00:04 +0530

    Refactor CORS middleware setup
    Updated CORS middleware configuration to allow all origins.

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,14 +18,19 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(
-  cors({
-    origin:*,
-  }),
-);
+// app.use(
+//   cors({
+//     origin:*,
+//   }),
+// );
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'POST,GET,LINK');
+});
 app.set('trust proxy', 1);
 
 app.get('/api/v2/health', (req, res) => {
