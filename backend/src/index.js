8647b3805b24f8c518cf0de894abf25528a236a8
Crazy Dev John <56544124+CrazyDevJohn@users.noreply.github.com>
2026-02-01 09:13:19 +0530

    Enable CORS middleware in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,15 +18,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-// app.use(
-//   cors({
-//     origin:*,
-//   }),
-// );
-
+app.use(
+  cors({
+    origin:*,
+  }),
+);
+app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'POST,GET,LINK');
